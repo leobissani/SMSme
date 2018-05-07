f6be3027f1edcd03fbb4ea1d5c0bfb59a1fd12e4
Leonardo <leobissani@gmail.com>
2018-05-07 14:57:45 -0300

    Initial commit

---
+++ app/src/main/java/smsme/bissani/smsme/activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package smsme.bissani.smsme.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import smsme.bissani.smsme.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/smsme/bissani/smsme/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package smsme.bissani.smsme.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import smsme.bissani.smsme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
