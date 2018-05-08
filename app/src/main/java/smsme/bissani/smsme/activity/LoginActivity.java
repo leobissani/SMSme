cf561a7dab8abb69b6e734cd9c5e38d47daf5fdf
Leonardo <leobissani@gmail.com>
2018-05-07 21:44:53 -0300

    login screen added

--- app/src/main/java/smsme/bissani/smsme/activity/LoginActivity.java
+++ app/src/main/java/smsme/bissani/smsme/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package smsme.bissani.smsme.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import smsme.bissani.smsme.R;
 
 public class LoginActivity extends AppCompatActivity {
--- app/src/main/java/smsme/bissani/smsme/activity/MainActivity.java
+++ app/src/main/java/smsme/bissani/smsme/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package smsme.bissani.smsme.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,10 +13,20 @@ import smsme.bissani.smsme.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        next = (Button) findViewById(R.id.button3);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
